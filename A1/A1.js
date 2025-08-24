ca368438e7e89c7f13245db363ec2af39a921fb0
Ammar Ali <ammarali02062003@gmail.com>
2025-08-25 02:02:11 +0500

    Assignment

---
+++ A1/A1.js
@@ -0,0 +1,75 @@
+// alert("Hello user")
+
+const button = document.getElementById("loginBtn");
+const password = document.getElementById("password")
+const userName = document.getElementById("username")
+
+// button.addEventListener('click', (e) => {
+
+//     e.preventDefault();
+
+//     if (password.value.trim() === '') {
+//         alert("Error! Please enter a vlaid Password")
+//     }
+// })
+
+// button.addEventListener('click', (e) => {
+//     e.preventDefault();
+
+//     if (password.value.trim() === '' && userName.value.trim() === '') {
+//         alert("Please enter the username & password")
+//     } else if (password.value.trim() === '') {
+//         alert("Please write the Password")
+//     } else if (userName.value.trim() === '') {
+//         alert("Please write the username")
+//     }else{
+//         alert("Welcome to JS Land...\nHappy Coding!")
+//         alert("Happy Coding!")
+//     }
+
+
+// })
+// -----------------Assignmet_02------------------
+
+// let userName1 = "Ali"
+
+// let myName = "Ammar"
+
+// alert(myName)  
+
+// let sName = "Ammar"
+// let sAge = 22
+// let certificate = "Web & App Development"
+
+// alert(sName)
+// alert(`${sAge} Years old`)
+// alert(certificate)
+
+// let pizza = "Pizza\nPizz\nPiz\nPi\nP"
+// alert(pizza)
+
+// let email  = "ammarali0206203@gmail.com"
+// alert(`My email Address is ${email}`)
+
+// let book = "A smarter way to learn Javascript"
+// alert(`I'm trying to learn from the book ${book}`)
+
+//----------------Assignment_03------------------
+
+// let age = 22
+// alert(`I am ${age} yaers old`)
+
+// let visit = localStorage.getItem("visitCount")
+
+// if (visit === null) {
+//     visit = 1;
+// } else {
+//     visit = Number(visit) + 1
+// }
+
+// localStorage.setItem("visitCount" , visit)
+
+// alert(`You have visited this site ${visit} times`)
+
+
+
---
+++ A2/app.js
@@ -0,0 +1,10 @@
+// let birthYear = 2003;
+
+// document.write(`My birth Year is ${birthYear}`, "<br />")
+// document.writeln("data type of my declared variable is  " + typeof(birthYear))
+
+let visitorName = "Fatir"
+let productTitle  = "T-shirt"
+let quantity = 5;
+
+document.writeln(`${visitorName} ordered ${quantity} ${productTitle} on H&S Clothing Store.`)
