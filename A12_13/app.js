8748437899855d035ae4f289c8717c03ff116318
Ammar Ali <ammarali02062003@gmail.com>
2025-08-31 15:40:42 +0500

    add A12_13

---
+++ A12_13/app.js
@@ -0,0 +1,72 @@
+// const userNum = prompt("Enter any character");
+
+// const asciiValue = userNum.charCodeAt(0);
+
+// if (asciiValue >= 48 && asciiValue <= 57) {
+//     console.log("Number");
+// } else if (asciiValue >= 65 && asciiValue <= 90) {
+//     console.log("Uppercase letter");
+// } else if (asciiValue >= 97 && asciiValue <= 122) {
+//     console.log("Lowercase letter");
+// } else {
+//     console.log("Not an alphabet");
+// }
+
+// const firstInt = +prompt("Enter First Integer")
+// const secondInt = +prompt("Enter Second Integer")
+
+// if (firstInt > secondInt) {
+//     console.log(`firstInt ${firstInt} is greater then secondInt ${secondInt}`);
+// } else if (firstInt < secondInt) {
+//     console.log(`firstInt ${firstInt} is less then secondInt ${secondInt}`);
+// } else {
+//     console.log(`firstInt ${firstInt} is equal to secondInt ${secondInt}`);
+// }
+
+// const num = +prompt("Enter a number");
+
+// if (num > 0) {
+//     console.log("The number is positive.");
+// } else if (num < 0) {
+//     console.log("The number is negative.");
+// } else {
+//     console.log("The number is zero.");
+// }
+
+// const char = prompt("Enter a character");
+// if (char.trim().length !== 1) {
+//     alert("Please enter a single character.");
+// } else if (
+//     char.toLowerCase().trim() === 'a' ||
+//     char.toLowerCase().trim() === 'e' ||
+//     char.toLowerCase().trim() === 'i' ||
+//     char.toLowerCase().trim() === 'o' ||
+//     char.toLowerCase().trim() === 'u'
+// ) {
+//     alert(true);
+// } else {
+//     alert(false);
+// }
+
+// const password  = "12547890" 
+
+// const askPassword = prompt("Enter your password");
+
+// if(askPassword.trim() === null || askPassword.trim() === ""){
+//     alert("Please enter your password");
+// } else if(askPassword === password){
+//     alert("Correct! The password enterd matches the orignal password");
+// } else {
+//     alert("Password is incorrect");
+// }
+
+// let greeting;
+// let hour = 13;
+// if (hour < 18) {
+//     greeting = "Good day";
+// } else {
+//     greeting = "Good evening";
+// }
+// console.log(greeting);
+
+
